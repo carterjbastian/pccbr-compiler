1ac8868c113dd522369207cb24c8ebfa6d849a22
Quinn Stearns <quinn@datahost.com>
2016-01-31 17:43:39 -0500

    Added test with more scopes

---
+++ parser/testfiles/scopeTest.c
@@ -0,0 +1,33 @@
+int cat = 6;
+int dog;
+
+void hello_world(void) {
+  int i, j = 2;
+  print "Hello world\n";
+  print "Goodbye world\n";
+  print "I sure hope this works\n";
+
+  if (x == 5) {
+    print "Welcome to a new cope";
+    for (i = 10; i < 14; ++i)  {
+      print "And another one";
+    }
+  }
+}
+
+int main(int a, int b, int c) {
+  int cat1, cat2 = 5;
+  int x = 5;
+  int y = 6;
+  int z = 7;
+  int q = 8;
+
+  if (1) {
+    print "we have scopes here too";
+  }
+
+  hello_world();
+  print "Hello World\n";
+  x = b + a / c;
+  return 0;
+}
