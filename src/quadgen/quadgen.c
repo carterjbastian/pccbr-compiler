7f57cfd64f6cd2fecbf99771e60ccc536063b2df
Carter J. Bastian <Carter.J.Bastian.17@Dartmouth.edu>
2016-02-14 18:26:07 -0500

    Added temporary variable generation

--- src/quadgen/quadgen.c
+++ src/quadgen/quadgen.c
@@ -1,10 +1,18 @@
 #include "quadgen.h"
+#include "../util/symtab.h"
 #include <string.h>
 #include <stdio.h>
 
+#define   MAX_TEMP_COUNT  1000000
+
+
 static const char *label_prefix = "L_";
+static const char *temp_prefix = "T";
+
+static int tempCount = 0;
 
 char *NewLabel(char *nodename, char *text);
+char *generate_temp_name();
 
 quad_t create_quad(optype op, symnode_t *operand1, symnode_t *operand2, symnode_t *operand3) {
   quad_t new_quad = calloc(1, sizeof(struct quad_struct));
@@ -38,6 +46,28 @@ void code_gen(ast_node node, symboltable_t *table) {
     code_gen(rsib, table);
   printf("%s\n", NODE_NAME(node->node_type));
   printf("%s\n", NewLabel(node->node_name, LABEL_NODE_NAME(node->node_type)));
+}
+
+/* 
+ * Creates a new temporary variable in the current scope of the program
+ */
+symnode_t *NewTemp(symboltable_t *table, var_lookup_type type) {
+  symnode_t *temp;
+  char *name = generate_temp_name();
+  // Notice that the line number doesn't apply here 
+  temp = insert_temp(table, type, name, 0); 
+  return temp;
+}
+
+char *generate_temp_name() {
+  assert(tempCount < MAX_TEMP_COUNT);
+
+  char *tName = calloc(1, sizeof(char) * 8);
+  assert(tName);
+  tName[7] = '\0';
+  sprintf(tName, "%s%d", temp_prefix, tempCount);
+  tempCount++;
+  return tName;
 }
 
 char *NewLabel(char *nodename, char *text) {
